b2dd6349c0c13a2d9928441e75868b522cf9ab71
Sadia Saman <1705102@ugrad.cse.buet.ac.bd>
2023-07-07 13:17:58 +0600

    Hard Mode added & UI modified

--- Src Code/ticTacToe.js
+++ Src Code/ticTacToe.js
@@ -1,73 +1,85 @@
 //declare constants
-const ComputerMove = 'X';
-const HumanMove = 'O';
+const ComputerPlayer = 'X';
+const HumanPlayer = 'O';
 
-const board = ["b1", "b2", "b3", "b4", "b5", "b6", "b7","b8","b9"]
-ComputerTurn = false;
-HumanTurn = true;
-boxValues = [];
-boxBtns = [];
+const board = ["b1", "b2", "b3", "b4", "b5", "b6", "b7","b8","b9"] 
 
+var EasyMode = undefined;
 
-function checkTie(){ 
-    for(box of board){
-        if(document.getElementById(box).value == '') return false; 
-    } 
-    disableBoard();
-    document.getElementById('print').innerHTML = "Match Tied";
+function setMediumMode(){
+    EasyMode = false;
+    clearBoard();
+    document.getElementById('mode').innerHTML = "Game is now in Hard Mode";
+}
+function setEasyMode(){
+    EasyMode = true;
+    clearBoard();
+    document.getElementById('mode').innerHTML = "Game is now in Easy Mode";
+}
+
+function checkTie(newBoard){ 
+    for(let i=0;i<9;i++){
+        if(newBoard[i] == '') return false; 
+    }  
     return true;
 }
-
-
-function disableBoard(){
-    for(box of board){
-        box.disabled = true;
-    }
-} 
-function changeColor(indices){
-    for(i of indices){
-        board[i].style.color = "red";
-    }
+function getBoard(){
+    let newBoard = [];
+    for(let i = 0; i < 9; i++){
+        newBoard.push(document.getElementById(board[i]).value);
+    } 
+    return newBoard;
 }
-
-
 function disableBoard(){
-    for(let i =0;i<9;i++){
+    boxBtns = [];
+    for(let i=0;i<9;i++){
+        boxBtns[i] = document.getElementById(board[i]);
         boxBtns[i].disabled = true;
-    }
+    } 
 } 
 function changeColor(indices){
+    boxBtns = [];
+    for(let i=0;i<9;i++){
+        boxBtns[i] = document.getElementById(board[i]);
+    }
     for(i of indices){
         boxBtns[i].style.color = "red";
     }
 } 
-function checkForWin(){
-    var winningCombination = [ [0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [2,5,8], [1,4,7]];
-    let i = 0;
-    for(box of board){
-        if(document.getElementById(box).value == '') boxValues[i++] = '_';
-        else boxValues[i++] = document.getElementById(box).value;
-    } 
-    console.log(boxValues);
-    for(let i = 0; i < winningCombination.length; i++){
-         if(isWinner(boxValues, winningCombination[i])){
-            disableBoard();
-            changeColor(winningCombination[i]);
-            return true;
+// Function called whenever user tab on any box
+function myfunc() { 
+    if(EasyMode===undefined){
+        document.getElementById('mode').innerHTML = "Default mode is Easy Mode";
+    }
+}
+
+function getMove(boxIndex){
+    document.getElementById(boxIndex).value = 'O';
+	document.getElementById(boxIndex).disabled = true;
+
+    if(EasyMode===true || EasyMode===undefined){
+        easyMode();
+        
+    }else{
+        MediumMode();
+        
+    }
+}
+function checkForWinner(newBoard, willWinnerDeclared){
+    var winningCombination = [ [0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [2,5,8], [1,4,7]]; 
+    for(let i = 0; i < winningCombination.length; i++){ 
+        var winner = isWinner(newBoard, winningCombination[i]);
+         if(winner != '_'  ){
+            if(willWinnerDeclared){
+                changeColor(winningCombination[i]);
+                disableBoard();
+            }
+            return winner;
          }
     }
     
-    return false;
+    return '_';
 }
-
-// Function called whenever user tab on any box
-function myfunc() { 
-    for(let i=0;i<9;i++){
-        boxBtns[i] = document.getElementById(board[i]);
-    }
-}
-
-
 
 function isWinner(boxValues, indices){
     var X_wins = true;
@@ -78,78 +90,148 @@ function isWinner(boxValues, indices){
         }
     }
     if(X_wins){
-        document.getElementById('print').innerHTML = "Player X won";
-        return true;
-    }
-    var O_wins = true;
-    for(index of indices){
-        if(boxValues[index] != 'O'){
-            O_wins = false;
-            break;
-        }
+         return 'X';
     }
     
-    if(O_wins){
-        document.getElementById('print').innerHTML = "Player O won";
-        return true;
-    }else{
-        return false;
-    }
+    for(index of indices){
+        if(boxValues[index] != 'O'){
+            return '_';
+        }
+    }   
+    return 'O';
 }
    
 
 // Function to reset game
-function ResetGame() {
-	location.reload(); 
+function clearBoard() {
+    for(boxIndex of board){
+        document.getElementById(boxIndex).value = '';
+	    document.getElementById(boxIndex).disabled = false;
+        document.getElementById(boxIndex).style.color = "black";
+    }
+}
+function ResetGame() { 
+	clearBoard();
+    console.log(EasyMode);
+    document.getElementById('printTurn').innerHTML ="Make the first move";
+    if(EasyMode===false){
+        setMediumMode();
+    }else{
+        setEasyMode();
+    }
+}
+function declareWinner(newBoard){
+    var winner = checkForWinner(newBoard, true);
+    if(winner != '_'){
+        if(winner === 'X'){
+            document.getElementById('printTurn').innerHTML ='Computer Won :(';
+        }else{
+            document.getElementById('printTurn').innerHTML ='You Won!';
+        }
+        return true;
+    }else if(checkTie(newBoard)) {
+        document.getElementById('printTurn').innerHTML ='Match Tied';
+        return true;
+    }
+    return false;
 }
  
-computerTurn = false;
-function easyMode(boxIndex){
-    console.log("easy mode " + boxIndex);
-    document.getElementById(boxIndex).value = "O";
-	document.getElementById(boxIndex).disabled = true;
-    if(checkForWin() || checkTie()) return;
+//------------------Easy Mode------------------//
+
+
+
+function easyMode(){
+    console.log("easy mode ");
+    if(declareWinner(getBoard())) return;
  
     let availableBoxes = [];
     for(let i = 0; i < 9; i++){
         if(document.getElementById(board[i]).value == ''){
             availableBoxes.push(board[i]);
         }
-    } 
+    }  
+    boxIndex = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];  
+    document.getElementById('printTurn').innerHTML ='Computer\'s Turn'
+    setTimeout(() => { console.log(""); }, 3000);
 
-    console.log("available boxes : "+availableBoxes.length)
-    boxIndex = availableBoxes[Math.floor(Math.random() * availableBoxes.length)]; 
-    console.log("random box selected : "+ boxIndex);
-    document.getElementById(boxIndex).value = "X";
+    document.getElementById(boxIndex).value = 'X';
     document.getElementById(boxIndex).disabled = true; 
-    console.log("computer turn : "+computerTurn);
-    if(checkForWin() || checkTie()) return;
+
+    if(declareWinner(getBoard())) return;
+    document.getElementById('printTurn').innerHTML ='Your Turn'
 }
 
-function bestMoveForMediumMode(){
-    let bestMove = -Infinity;
-    let chosenBox;
-    for(let i=0;i<9;i++){
-        if(document.getElementById(board[i]).value == ''){ 
-            board[i] = 'X'  ;        
-            let score = minimax(board,computerTurn, ); 
-            board[i]='';
-            if(score > bestMove){
-                bestMove = score;
-                chosenBox = b;
+
+ //-----------------------------Medium Mode--------------------------------
+
+function MediumMode(){
+    console.log("Medium mode " + boxIndex);  
+    if(declareWinner(getBoard())) return; 
+
+    document.getElementById('printTurn').innerHTML ='Computer\'s Turn';
+    
+    boxIndex = chooseBestMove();
+    document.getElementById(board[boxIndex]).value = "X";
+    document.getElementById(board[boxIndex]).disabled = true; 
+    
+    if(declareWinner(getBoard())) return; 
+    document.getElementById('printTurn').innerHTML ='Your Turn';
+}
+
+function chooseBestMove(){ 
+    var choosenBox ;
+    let newBoard = getBoard();
+    let bestScore = -Infinity;
+    
+    for(let i = 0; i < 9; i++){
+        if(newBoard[i] == ''){
+            newBoard[i] = ComputerPlayer;
+            let score = minimax(newBoard,0, HumanPlayer); 
+            newBoard[i] = '';
+            if(score > bestScore){
+                bestScore = score;
+                choosenBox = i;
             }
         }
-    }
-    return chosenBox;
+    } 
+    console.log("result from minimax "+bestScore+" "+ choosenBox);
+    return choosenBox; 
 }
-
-function mediumMode(){
-
-}
-
-
-function minimax(boxes, ){
-
-}
-
  
+
+
+function minimax(newBoard,depth, player){ 
+    var winner = checkForWinner(newBoard, false);
+    if(winner == ComputerPlayer){
+        return 10;
+    }else if(winner == HumanPlayer){
+        return -10;
+    }else if(checkTie(newBoard)){
+        return 0;
+    }
+    if(player == ComputerPlayer){
+        let score = -Infinity;
+        for(let i=0;i<9;i++){
+            if(newBoard[i]==''){
+                newBoard[i] = player; 
+                score = Math.max(minimax(newBoard,depth+1, HumanPlayer), score);
+                //console.log("max score "+score);
+                newBoard[i] = '';
+            }
+        } 
+        return score;
+    }else{
+        let score = Infinity;
+        for(let i=0;i<9;i++){
+            if(newBoard[i]==''){
+                newBoard[i] = player; 
+                score = Math.min(minimax(newBoard, depth+1, ComputerPlayer), score);
+                //console.log("min score "+score);
+                newBoard[i] = '';
+            }
+        } 
+        return score;
+    }
+    
+}
+
